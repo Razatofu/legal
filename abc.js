cf3cb82667d19dddc8df9f5477777e7bcd18f5dd
raza ayaz <razaayaz@yahoo.com>
2024-12-24 22:07:56 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,5 @@
+var x = 10, y = 20, name = "Alice", isActive = true, count = 5;
+let x = 10, y = 20, name = "Alice", isActive = true, count = 5;
+var myvar= legal varible;
+console.log(myvar);
+document.write(myvar);
